43f368771e75c0debd07505a4b818681d49d401d
Rian Anugrah <rian.te.quiero@gmail.com>
2021-12-17 20:14:07 +0700

    Builerplate

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,16 @@
+<div class="w-screen h-screen bg-slate-300 p-2 flex flex-col">
+  <div class="w-full  p-4 bg-white shadow">
+    <a href="/" class="bg-slate-400 rounded-md border p-2 text-center active:text-white">Home</a>
+    <a href="/add" class="bg-slate-400 rounded-md border p-2 text-center active:text-white">Add</a>
+  </div>
+
+  <div class="mt-4">
+    <slot />
+  </div>
+</div>
+
+<style>
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+</style>
---
+++ src/routes/add.svelte
@@ -0,0 +1,27 @@
+<script>
+  import supabase from '$lib/db';
+  let newGame;
+  let submit = false;
+  async function sendData() {
+    const { data, error } = await supabase
+      .from('games')
+      .insert([{ name: newGame }]);
+    if (error) throw new Error(error.message);
+    return data;
+  }
+</script>
+
+<form on:submit|preventDefault={() => (submit = true)}>
+  <input type="text" bind:value={newGame} />
+  <input type="submit" value="Submit" on:click={() => (submit = false)} />
+</form>
+{#if submit}
+  {#await sendData()}
+    <p>Sending data...</p>
+  {:then data}
+    <p>Successfully sent data.</p>
+  {:catch error}
+    <p>Something went wrong while sending the data:</p>
+    <pre>{error}</pre>
+  {/await}
+{/if}
